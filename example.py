53227b7a09fa9bbe93e5d320ecfabce90070f193
Vařa <vojtavara06@email.cz>
2024-04-03 11:47:20 +0200

    example nearly working

--- example.py
+++ example.py
@@ -1,12 +1,13 @@
 """ example script showing how you can run things"""
 
+from datetime import date, timedelta
 import json
 from functools import lru_cache
 
 # copy config.py.example to config.py and fill in your details
 from config import args
 
-from pygoodwe import SingleInverter
+from pygoodwe import API, SingleInverter
 
 @lru_cache()
 def get_single_inverter(args=args): #pylint: disable=redefined-outer-name,dangerous-default-value
@@ -17,7 +18,7 @@ def get_single_inverter(args=args): #pylint: disable=redefined-outer-name,danger
             account=args.get('gw_account', 'thiswillnotwork'),
             password=args.get('gw_password', 'thiswillnotwork'),
             )
-    # print("Grabbing data")
+    print("Grabbing data")
     goodwe.getCurrentReadings()
     return goodwe
 
@@ -26,19 +27,19 @@ def test_get_temperature():
     goodwe=get_single_inverter()
     assert goodwe.get_inverter_temperature()
 
-# print("Multi Inverter")
-# gw = API(
-#         system_id=args.get('gw_station_id', '1'),
-#         account=args.get('gw_account', 'thiswillnotwork'),
-#         password=args.get('gw_password', 'thiswillnotwork'),
-#         )
-# # print("Grabbing data")
-# gw.getCurrentReadings(raw=True)
+print("Multi Inverter")
+gw = API(
+        system_id=args.get('gw_station_id', '1'),
+        account=args.get('gw_account', 'thiswillnotwork'),
+        password=args.get('gw_password', 'thiswillnotwork'),
+        )
+print("Grabbing data")
+gw.getCurrentReadings(raw=True)
 
-# print(f"Temperature: {gw.get_inverter_temperature()}")
+print(f"Temperature: {gw.get_inverter_temperature()}")
 
 # battery state of charge
-#print(f"Current SOC: {gw.get_battery_soc()}")
+# print(f"Current SOC: {gw.get_battery_soc()}")
 
 inverter = get_single_inverter()
 print(f"Available fields in data: {inverter.data.keys()}")
@@ -47,15 +48,15 @@ batterydata = inverter.data.get('inverter',{}).get('battery',"").split("/")
 if batterydata:
     voltage = float(batterydata[0][:-1])
     print(f"Battery voltage is: {voltage}")
-#print(json.dumps(gw.data))
-#print(f"Are the batteries full? {gw.are_batteries_full(fullstate=90.0)}")
+print(json.dumps(gw.data))
+print(f"Are the batteries full? {gw.are_batteries_full(fullstate=90.0)}")
 
-#print(gw.data.keys())
-#print(json.dumps(gw.data['inverter']))
-#print(json.dumps(gw.data))
+print(gw.data.keys())
+print(json.dumps(gw.data['inverter']))
+print(json.dumps(gw.data))
 
-# print(f"PV Flow: {gw.getPVFlow()}")
-# print(f"Voltage: {gw.getVoltage()}")
+#print(f"PV Flow: {gw.getPVFlow()}")
+print(f"Voltage: {gw.getVoltage()}")
 
-# print("Getting XLS file - this doesn't work!")
-# inverter.getDayDetailedReadingsExcel(date.today() - timedelta(days=1))
+#print("Getting XLS file - this doesn't work!")
+#inverter.getDayDetailedReadingsExcel(date.today() - timedelta(days=1))
